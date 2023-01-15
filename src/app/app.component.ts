a01014a9867cce4b0066ef7991e8bbb69fb8589b
kkalecki <kkalecki@consdata.com>
2023-01-15 15:51:13 +0100

    Dodanie komunikacji pomiedzy komponentami

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  displayRecipes: boolean = true;
+  displayShoppingList: boolean = false;
+
+  onDisplayRecipes($event: boolean) {
+    this.displayRecipes = $event;
+  }
+
+  onDisplayShoppingList($event: boolean) {
+    this.displayShoppingList = $event;
+  }
 }
